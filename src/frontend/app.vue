962e9d04787d6b1be3034d2f6664a82fc2c91f87
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-06 14:16:25 +0100

    Add less to the project

--- src/frontend/app.vue
+++ src/frontend/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Sita van je Osso!</h1>
   </div>
 </template>
+
+<style lang="less" scoped>
+h1 {
+  color: @color-red;
+}
+</style>
--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -1,5 +1,23 @@
+import path from 'path'
+import svgLoader from 'vite-svg-loader'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
   devtools: { enabled: true },
+  css: ['@/assets/css/main.less'],
+
+  vite: {
+    plugins: [svgLoader()],
+    css: {
+      preprocessorOptions: {
+        less: {
+          additionalData: `
+          @import "${path.resolve(__dirname, './assets/css/vars.less')}";
+          `,
+          math: 'always',
+        },
+      },
+    },
+  },
 })
