d4521aacc7c4e5a9127412bd4f874ca4f71efb25
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-06 15:10:50 +0100

    Add setup for typography component

--- src/frontend/components/shared/Typography/Typography.stories.ts
+++ src/frontend/components/shared/Typography/Typography.stories.ts
@@ -1,109 +1,90 @@
 import type { Meta, StoryFn } from '@storybook/vue3'
 
-import Typography, { TypographyProps } from './index.vue'
+import Typography from './index.vue'
+
+const variantsArray = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body', 'body-large']
 
 export default {
-  title: 'Shared/Shared/Typography',
+  title: 'Shared/Typography',
   component: Typography,
+  tags: ['autodocs'],
+  argTypes: {
+    classes: {
+      table: { disable: true },
+    },
+    as: {
+      table: { disable: true },
+    },
+    variant: {
+      options: variantsArray,
+    },
+  },
 } as Meta<typeof Typography>
 
-const Template: StoryFn<typeof Typography> = (args: TypographyProps) => ({
+const Template: StoryFn<typeof Typography> = (args: any) => ({
   components: { Typography },
   setup() {
-    return { args }import type { Meta, StoryFn } from '@storybook/vue3'
-
-    import Typography from './index.vue'
-    
-    const variantsArray = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body', 'body-large']
-    
-    export default {
-      title: 'Shared/Typography',
-      component: Typography,
-      tags: ['autodocs'],
-      argTypes: {
-        classes: {
-          table: { disable: true },
-        },
-        as: {
-          table: { disable: true },
-        },
-        variant: {
-          options: variantsArray,
-        },
-      },
-    } as Meta<typeof Typography>
-    
-    const Template: StoryFn<typeof Typography> = (args: any) => ({
-      components: { Typography },
-      setup() {
-        return { args }
-      },
-      template: `<Typography v-bind="args">${args.default}</Typography>`,
-    })
-    
-    export const Heading1 = Template.bind({})
-    Heading1.args = {
-      variant: 'h1',
-      default: 'Heading 1',
-    }
-    
-    export const Heading2 = Template.bind({})
-    Heading2.args = {
-      variant: 'h2',
-      default: 'Heading 2',
-    }
-    
-    export const Heading3 = Template.bind({})
-    Heading3.args = {
-      variant: 'h3',
-      default: 'Heading 3',
-    }
-    
-    export const Heading4 = Template.bind({})
-    Heading4.args = {
-      variant: 'h4',
-      default: 'Heading 4',
-    }
-    
-    export const Heading5 = Template.bind({})
-    Heading5.args = {
-      variant: 'h5',
-      default: 'Heading 5',
-    }
-    
-    export const Heading6 = Template.bind({})
-    Heading6.args = {
-      variant: 'h6',
-      default: 'Heading 6',
-    }
-    
-    export const BodyLarge = Template.bind({})
-    BodyLarge.args = {
-      variant: 'body-large',
-      default: 'Body large',
-    }
-    
-    export const Body = Template.bind({})
-    Body.args = {
-      variant: 'body',
-      default: 'Body',
-    }
-    
-    export const Label = Template.bind({})
-    Label.args = {
-      variant: 'label',
-      default: 'Label',
-    }
-    
-    export const LabelBig = Template.bind({})
-    LabelBig.args = {
-      variant: 'label-big',
-      default: 'Label Big',
-    }
-    
+    return { args }
   },
-  template: '<Typography v-bind="args" />',
+  template: `<Typography v-bind="args">${args.default}</Typography>`,
 })
 
-export const TypographyStory = Template.bind({})
-TypographyStory.args = {}
+export const Heading1 = Template.bind({})
+Heading1.args = {
+  variant: 'h1',
+  default: 'Heading 1',
+}
+
+export const Heading2 = Template.bind({})
+Heading2.args = {
+  variant: 'h2',
+  default: 'Heading 2',
+}
+
+export const Heading3 = Template.bind({})
+Heading3.args = {
+  variant: 'h3',
+  default: 'Heading 3',
+}
+
+export const Heading4 = Template.bind({})
+Heading4.args = {
+  variant: 'h4',
+  default: 'Heading 4',
+}
+
+export const Heading5 = Template.bind({})
+Heading5.args = {
+  variant: 'h5',
+  default: 'Heading 5',
+}
+
+export const Heading6 = Template.bind({})
+Heading6.args = {
+  variant: 'h6',
+  default: 'Heading 6',
+}
+
+export const BodyLarge = Template.bind({})
+BodyLarge.args = {
+  variant: 'body-large',
+  default: 'Body large',
+}
+
+export const Body = Template.bind({})
+Body.args = {
+  variant: 'body',
+  default: 'Body',
+}
+
+export const Label = Template.bind({})
+Label.args = {
+  variant: 'label',
+  default: 'Label',
+}
+
+export const LabelBig = Template.bind({})
+LabelBig.args = {
+  variant: 'label-big',
+  default: 'Label Big',
+}
--- src/frontend/components/shared/Typography/index.vue
+++ src/frontend/components/shared/Typography/index.vue
@@ -82,39 +82,30 @@ const classes = computed(() => {
 
 <style lang="less" scoped>
 .heading-1 {
-  .heading-1();
 }
 
 .heading-2 {
-  .heading-2();
 }
 
 .heading-3 {
-  .heading-3();
 }
 
 .heading-4 {
-  .heading-4();
 }
 
 .heading-5,
 .heading-6 {
-  .heading-5();
 }
 
 .body {
-  .body();
 }
 
 .body-large {
-  .body-large();
 }
 
 .label {
-  .label();
 }
 
 .label-big {
-  .label-big();
 }
 </style>
