7ad5600064651207b9fb7592d728114e1fb722d8
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-06 10:13:37 +0100

    Add ESlint

---
+++ src/frontend/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['prettier', 'no-relative-import-paths'],
+  rules: {
+    curly: 'error',
+    'vue/multi-word-component-names': 0,
+    'prettier/prettier': 'error',
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+    'no-relative-import-paths/no-relative-import-paths': [
+      'warn',
+      {
+        allowSameFolder: true,
+        prefix: '@',
+      },
+    ],
+  },
+}
