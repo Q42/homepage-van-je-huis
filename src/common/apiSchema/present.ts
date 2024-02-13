aefdc56862ec6cab4ea247e3ae7d5161cc5939d6
Thomas Broch <37216178+ThomasBroch@users.noreply.github.com>
2024-02-13 15:48:44 +0100

    addressed pr comments

--- src/common/apiSchema/present.ts
+++ src/common/apiSchema/present.ts
@@ -3,42 +3,40 @@ import { BaseView, BaseDataEntity, BaseSliderEntry, ImageRef } from "./shared";
 export type GeoLevel = "straat" | "buurt" | "wijk" | "stadsdeel";
 
 export type PresentEntityType =
-    | "outdoorArt"
-    | "cultureMulti"
-    | "musicVenue"
-    | "heritage"
-    | "dance"
-    | "visualArts"
-    | "photography"
-    | "cinema"
-    | "literary"
-    | "creativeIndustries"
-    | "theatre"
-    | "TVandRadio"
-    | "architecture"
-    | "newMedia"
-    | "debate";
+  | "outdoorArt"
+  | "cultureMulti"
+  | "musicVenue"
+  | "heritage"
+  | "dance"
+  | "visualArts"
+  | "photography"
+  | "cinema"
+  | "literary"
+  | "creativeIndustries"
+  | "theatre"
+  | "TVandRadio"
+  | "architecture"
+  | "newMedia"
+  | "debate";
 
 export interface PresentData extends BaseView {
-    slider: DistanceViewEntry[]; // The entries for on the slider
-    agenda: AgendaItem[]; // The events and such
+  slider: DistanceViewEntry[]; // The entries for on the slider
+  agenda: AgendaItem[]; // The events and such
 }
 
 export interface DistanceViewEntry extends BaseSliderEntry {
-    position: number; // distance from the address
-    geoLevel?: GeoLevel; // The geo level of the entry
-    image?: ImageRef;
-    title: string; // Title of the entry
-    visitUrl?: string; // URL to the source where people can find out more info about the image
-    type: PresentEntityType; // The type of the entry
+  geoLevel?: GeoLevel; // The geo level of the entry
+  image?: ImageRef;
+  visitUrl?: string; // URL to the source where people can find out more info about the image
+  type: PresentEntityType; // The type of the entry
 }
 
 export interface AgendaItem extends BaseDataEntity {
-    image?: ImageRef; // Hero image for the event
-    title: string; // Title of the event
-    description: string; // Description of the event
-    date: Date; // Date of the event
-    dateEnd?: Date; // End date of the event
-    visitUrl?: string; // URL to the source where people can find out more info about the event
-    visitUrlText?: string; // Text to display for the visitUrl. If none is provided, the frontend should default to the default copy/
+  image?: ImageRef; // Hero image for the event
+  title: string; // Title of the event
+  description: string; // Description of the event
+  date: Date; // Date of the event
+  dateEnd?: Date; // End date of the event
+  visitUrl?: string; // URL to the source where people can find out more info about the event
+  visitUrlText?: string; // Text to display for the visitUrl. If none is provided, the frontend should default to the default copy/
 }
--- src/common/apiSchema/shared.ts
+++ src/common/apiSchema/shared.ts
@@ -1,23 +1,24 @@
 import { PresentEntityType } from "./present";
 
 export type BaseDataEntity = {
-    sourceId?: string;
-    source?: string;
+  sourceId?: string;
+  source?: string;
 };
 
 export interface BaseSliderEntry extends BaseDataEntity {
-    position: number;
-    type: PresentEntityType | "archiveImage";
+  title: string; // Title of the entry
+  position: number;
+  type: PresentEntityType | "archiveImage";
 }
 
 export type BaseView = {
-    rangeStart: number; // Meters at which the slider starts
-    rangeEnd: number; // Meters at which the slider starts
+  rangeStart: number; // Meters at which the slider starts
+  rangeEnd: number; // Meters at which the slider starts
 };
 
 export type ImageRef = {
-    url: string;
-    width?: number;
-    height?: number;
-    altText?: string;
+  url: string;
+  width?: number;
+  height?: number;
+  altText?: string;
 };
