5c92bcf3e1bece068f22a0076a9a1842790e70b5
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-13 13:56:11 +0100

    Expand addresService

--- src/frontend/utils/addressService.ts
+++ src/frontend/utils/addressService.ts
@@ -1,10 +1,22 @@
 export const getAddressJSONandParse = async (id: string) => {
-  const filePath = `/mockdata/addressdata/${id}.json`
+  const filePath = `/mockdata/address/${id}.json`
   const response = await fetch(filePath)
   if (!response.ok) {
-    console.log('Error fetching address data')
+    console.error('404 - Adress not found')
     return null
   }
   const jsonData = await response.json()
   return jsonData
+}
+
+// TODO: fix typing
+export const getImagesViewModel = (slider: any[]) => {
+  return slider.map((sliderItem: any) => {
+    return {
+      src: sliderItem.url,
+      alt: sliderItem.alt,
+      title: sliderItem.title,
+      visitUrl: sliderItem.visitUrl,
+    }
+  })
 }
