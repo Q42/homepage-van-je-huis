1ad75c5fffc844411b0a37c946c860b1d467714d
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-13 21:36:25 +0100

    Add the panel hover effect

--- src/frontend/components/UI/SidePanel/index.vue
+++ src/frontend/components/UI/SidePanel/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="side-panel">
-    <div class="side-panel__label">
+  <div class="side-panel" :class="{ 'side-panel--hover': isHovering }">
+    <button
+      @mouseenter="handleMousEnter"
+      @mouseleave="handleMouseLeave"
+      class="side-panel__label"
+    >
       <SharedIcon type="stories" :height="24" :width="24" />
       <SharedTypography
         classes="side-panel__label__text"
@@ -8,7 +12,7 @@
         :compact="true"
         >verhalen</SharedTypography
       >
-    </div>
+    </button>
   </div>
 </template>
 
@@ -17,7 +21,24 @@ export interface SidePanelProps {
   // TODO
 }
 
+const isHovering = ref(false)
+let timeout: NodeJS.Timeout | null = null
+
 const props = defineProps<SidePanelProps>()
+
+const handleMousEnter = () => {
+  if (timeout) {
+    clearTimeout(timeout)
+    timeout = null
+  }
+  isHovering.value = true
+}
+
+const handleMouseLeave = () => {
+  timeout = setTimeout(() => {
+    isHovering.value = false
+  }, 700)
+}
 </script>
 
 <style lang="less" scoped>
@@ -30,20 +51,30 @@ const props = defineProps<SidePanelProps>()
   z-index: 1;
   background: @primary-black;
   transform: translateX(90%);
+  transition: transform 0.3s;
+}
+
+.side-panel--hover {
+  transform: translateX(80%);
 }
 
 .side-panel__label {
+  all: unset;
+  outline: none;
   height: 48px;
   width: fit-content;
   display: flex;
   align-items: center;
   justify-content: center;
   color: @primary-white;
-  padding: 20px;
+  padding-left: 20px;
+  padding-right: 100vw;
   background: @primary-black;
-  transform: translate(calc(-100% + 1px), 12px);
+  transform: translate(calc(-100% + 100vw - 20px), 12px);
   gap: 1rem;
+  cursor: pointer;
 }
+
 .side-panel__label__text {
   color: @primary-white;
 }
