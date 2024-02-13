357feb9618d2410290a4fca45ce018e55cbb7507
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-13 15:41:42 +0100

    Add link

---
+++ src/frontend/components/shared/Link/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLink :to="href" class="link"
+    >{{ label }} <span><SharedIcon :height="13" :width="13" type="link" /></span
+  ></NuxtLink>
+</template>
+
+<script setup lang="ts">
+export interface LinkProps {
+  href: string
+  label: string
+}
+
+const props = defineProps<LinkProps>()
+</script>
+
+<style lang="less" scoped>
+.link {
+  display: inline-flex;
+  gap: 5px;
+  align-items: center;
+  color: inherit;
+  text-decoration: none;
+  cursor: pointer;
+  border-bottom: 1px solid @primary-black;
+  &:hover {
+    border-bottom: 2px solid @primary-black;
+  }
+}
+</style>
