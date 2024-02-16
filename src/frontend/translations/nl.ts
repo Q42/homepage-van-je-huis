e41c11dd1d91b751466850004e67eb9099aaf700
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-16 11:20:43 +0100

    Add input placeholders

--- src/frontend/translations/nl.ts
+++ src/frontend/translations/nl.ts
@@ -2,7 +2,8 @@ export const nl = {
   home: {
     title: 'Homepage van je Huis',
     subtitle: 'Ontdek het verleden en heden van je huis en de buurt.',
-    inputPlaceholder: 'Zoek een adres',
+    streetInputPlaceHolder: 'Zoek een straatnaam',
+    houseNumberInputPlaceHolder: 'Huisnummer',
   },
   images: {
     externalLink: 'Naar originele bron',
