cc68f6766950a5ab3c9b69941edaae5f999b2369
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-02-26 15:24:54 +0100

    Fix the deploy script

--- src/frontend/deploy.sh
+++ src/frontend/deploy.sh
@@ -1,3 +1,3 @@
-npm run i
+npm i
 npm run generate
 swa deploy .output/public --api-language node --deployment-token d4c918b2bada1c9254b1794be946eddc09f98f3414b96ccb4a094491c59989d14-91345953-f6e2-4278-8ca5-cbd0a42f4b8d003377080 --env production --verbose silly
