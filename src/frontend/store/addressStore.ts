5de605144c822d96ec842355a1fdd5f8e8c07846
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-03-01 16:21:59 +0100

    Make store of address composable

--- src/frontend/store/addressStore.ts
+++ src/frontend/store/addressStore.ts
@@ -1,22 +1,32 @@
+import { defineStore } from 'pinia'
 import { PastData } from '../../common/apiSchema/past'
 import { AddressRecord } from '../../common/apiSchema/addressRecord'
-
 import { PresentData } from '../../common/apiSchema/present'
 
-// TODO: make this a store to prevent lots of requests
-export const useAddressStore = (address: string) => {
+export const useAddressStore = defineStore('address', () => {
   const addressService = useAddressService()
-
   const addressData: Ref<AddressRecord | null> = ref(null)
   const pastData: Ref<PastData | null> = ref(null)
   const presentData: Ref<PresentData | null> = ref(null)
 
-  onMounted(async () => {
+  const fetchAddressData = async (address: string) => {
     const data = await addressService.getAddressJSONandParse(address)
     addressData.value = data
     pastData.value = data.pastData
     presentData.value = data.presentData
-  })
+  }
 
-  return { addressData, pastData, presentData }
-}
+  const resetAddressData = () => {
+    addressData.value = null
+    pastData.value = null
+    presentData.value = null
+  }
+
+  return {
+    addressData,
+    pastData,
+    presentData,
+    fetchAddressData,
+    resetAddressData,
+  }
+})
