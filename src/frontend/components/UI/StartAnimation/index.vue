0506ae2a69f2a527ca940e56444bfe74b17b0618
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-03-08 16:07:19 +0100

    Fix the start animation

--- src/frontend/components/UI/StartAnimation/index.vue
+++ src/frontend/components/UI/StartAnimation/index.vue
@@ -1,29 +1,19 @@
 <template>
-  <div class="start-animation">
+  <div v-if="randomImages" class="start-animation">
     <SharedImage
-      :image="{ url: getRandomImageUrl() }"
+      v-for="(image, index) in randomImages"
+      :key="index"
+      :image="{ url: image }"
       class="animate-item"
-    ></SharedImage>
-    <SharedImage
-      :image="{ url: getRandomImageUrl() }"
-      class="animate-item"
-    ></SharedImage>
-    <SharedImage
-      :image="{ url: getRandomImageUrl() }"
-      class="animate-item"
-    ></SharedImage>
-    <SharedImage
-      :image="{ url: getRandomImageUrl() }"
-      class="animate-item"
-    ></SharedImage>
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import gsap from 'gsap'
-import { isMobile } from '@/utils/breakpoints'
 
 const imagesIds = Array.from({ length: 35 }, (_, i) => i + 1)
+const between = ref(false)
 
 const getRandomImageUrl = () => {
   if (imagesIds.length === 4) {
@@ -37,57 +27,89 @@ const getRandomImageUrl = () => {
   return `/startanimation/${pickedItem}.jpg`
 }
 
-onMounted(() => {
+let randomImages: Ref<string[] | null> = ref(null)
+
+const setRandomImages = () => {
+  console.log('setRandomImages')
+  randomImages.value = Array.from({ length: 4 }, () => getRandomImageUrl())
+  console.log('randomImages', randomImages.value)
+}
+
+const animate = async () => {
+  setRandomImages()
+  await nextTick()
+
   const items = document.getElementsByClassName('animate-item')
   const windowHeight = window.innerHeight
   const windowWidth = window.innerWidth
 
-  setTimeout(() => {
-    Array.from(items).forEach((el, index) => {
-      const elementWidth = el.clientWidth
-      const elementHeight = el.clientHeight
+  Array.from(items).forEach((el, index) => {
+    const elementWidth = el.clientWidth
+    const elementHeight = el.clientHeight
 
-      const fromPositions = [
-        { x: -elementWidth, y: -elementHeight }, // top left
-        {
-          x: windowWidth + elementWidth,
-          y: -elementHeight,
-        }, // top right
-        { x: -elementWidth, y: windowHeight + elementHeight }, // bottom left
-        { x: windowWidth + elementWidth, y: windowHeight + elementHeight }, // bottom right
-      ]
+    const fromPositions = [
+      { x: -elementWidth, y: -elementHeight }, // top left
+      {
+        x: windowWidth + elementWidth,
+        y: -elementHeight,
+      }, // top right
+      { x: -elementWidth, y: windowHeight + elementHeight }, // bottom left
+      { x: windowWidth + elementWidth, y: windowHeight + elementHeight }, // bottom right
+    ]
 
-      const highest = isTablet(windowWidth) ? 160 : 210
-      const lowest = highest - 140
+    const highest = isTablet(windowWidth) ? 160 : 210
+    const lowest = highest - 140
 
-      const toPositions = [
-        { x: windowWidth * 0.1, y: lowest }, // top left
-        { x: windowWidth - elementWidth - windowWidth * 0.1, y: highest }, // top right
-        { x: windowWidth * 0.1, y: windowHeight - elementHeight - highest }, // bottom left
-        {
-          x: windowWidth - elementWidth - windowWidth * 0.1,
-          y: windowHeight - elementHeight - lowest,
-        }, // bottom right
-      ]
+    const toPositions = [
+      { x: windowWidth * 0.1, y: lowest }, // top left
+      { x: windowWidth - elementWidth - windowWidth * 0.1, y: highest }, // top right
+      { x: windowWidth * 0.1, y: windowHeight - elementHeight - highest }, // bottom left
+      {
+        x: windowWidth - elementWidth - windowWidth * 0.1,
+        y: windowHeight - elementHeight - lowest,
+      }, // bottom right
+    ]
 
-      gsap.to(el, {
-        x: toPositions[index].x,
-        y: toPositions[index].y,
-        opacity: 1,
-        duration: 2,
-      })
+    gsap.to(el, {
+      x: toPositions[index].x,
+      y: toPositions[index].y,
+      opacity: 1,
+      duration: 2,
+    })
 
-      setTimeout(() => {
-        gsap.to(el, {
+    setTimeout(() => {
+      gsap
+        .to(el, {
           x: fromPositions[index].x,
           y: fromPositions[index].y,
           opacity: 0,
           duration: 1,
         })
-      }, 5000)
-    })
-  }, 2000)
-})
+        .then(() => {
+          console.log('Je Moer')
+          randomImages.value = null
+        })
+    }, 5000)
+  })
+}
+
+let count = 2
+let interval: NodeJS.Timeout | null = null
+
+const startAnimation = () => {
+  setTimeout(() => {
+    animate()
+    interval = setInterval(() => {
+      if (count === 5 && interval) {
+        clearInterval(interval)
+      }
+      animate()
+      count += 1
+    }, 8000)
+  }, 1000)
+}
+
+onMounted(startAnimation)
 
 export interface StartAnimationProps {
   // TODO
