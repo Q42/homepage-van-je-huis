d14de3c9b567c33ac2775d8f1836fd401ce70d28
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-03-11 17:34:10 +0100

    New startpositions

--- src/frontend/components/UI/StartAnimation/index.vue
+++ src/frontend/components/UI/StartAnimation/index.vue
@@ -32,80 +32,7 @@ const setRandomImages = () => {
   randomImages.value = Array.from({ length: 4 }, () => getRandomImageUrl())
 }
 
-const animate = async () => {
-  setRandomImages()
-  await nextTick()
-
-  const items = document.getElementsByClassName('animate-item')
-  const windowHeight = window.innerHeight
-  const windowWidth = window.innerWidth
-
-  Array.from(items).forEach((el, index) => {
-    const elementWidth = el.clientWidth
-    const elementHeight = el.clientHeight
-
-    const fromPositions = [
-      { x: -elementWidth, y: -elementHeight }, // top left
-      {
-        x: windowWidth + elementWidth,
-        y: -elementHeight,
-      }, // top right
-      { x: -elementWidth, y: windowHeight + elementHeight }, // bottom left
-      { x: windowWidth + elementWidth, y: windowHeight + elementHeight }, // bottom right
-    ]
-
-    const highest = isTablet(windowWidth) ? 160 : 210
-    const lowest = highest - 140
-
-    const toPositions = [
-      { x: windowWidth * 0.1, y: lowest }, // top left
-      { x: windowWidth - elementWidth - windowWidth * 0.1, y: highest }, // top right
-      { x: windowWidth * 0.1, y: windowHeight - elementHeight - highest }, // bottom left
-      {
-        x: windowWidth - elementWidth - windowWidth * 0.1,
-        y: windowHeight - elementHeight - lowest,
-      }, // bottom right
-    ]
-
-    gsap.to(el, {
-      x: toPositions[index].x,
-      y: toPositions[index].y,
-      opacity: 1,
-      duration: 2,
-    })
-
-    setTimeout(() => {
-      gsap
-        .to(el, {
-          x: fromPositions[index].x,
-          y: fromPositions[index].y,
-          opacity: 0,
-          duration: 1,
-        })
-        .then(() => {
-          randomImages.value = null
-        })
-    }, 5000)
-  })
-}
-
-let count = 2
-let interval: NodeJS.Timeout | null = null
-
-const startAnimation = () => {
-  setTimeout(() => {
-    animate()
-    interval = setInterval(() => {
-      if (count === 5 && interval) {
-        clearInterval(interval)
-      }
-      animate()
-      count += 1
-    }, 8000)
-  }, 1000)
-}
-
-onMounted(startAnimation)
+onMounted(setRandomImages)
 </script>
 
 <style lang="less" scoped>
@@ -124,7 +51,7 @@ onMounted(startAnimation)
   width: 30%;
   min-width: 180px;
   aspect-ratio: 3/2;
-  opacity: 0;
+  opacity: 1;
 
   @media @mq-from-desktop-md {
     width: 20%;
@@ -132,18 +59,18 @@ onMounted(startAnimation)
 }
 
 .animate-item:nth-child(1) {
-  transform: translate(-100%, -100%);
+  transform: translate(0, 0);
 }
 
 .animate-item:nth-child(2) {
-  transform: translate(calc(100vw + 100%), -100%);
+  transform: translate(calc(100vw - 100%), 0);
 }
 
 .animate-item:nth-child(3) {
-  transform: translate(-100%, calc(100vh + 100%));
+  transform: translate(0, calc(100vh - 100%));
 }
 
 .animate-item:nth-child(4) {
-  transform: translate(calc(100vw + 100%), calc(100vh + 100%));
+  transform: translate(calc(100vw - 100%), calc(100vh - 100%));
 }
 </style>
