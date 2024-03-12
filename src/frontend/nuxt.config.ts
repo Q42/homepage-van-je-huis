35d7b827e7e04344520761c297f2f9932f0457c4
Len Verstijnen <35574315+lenverstijnen@users.noreply.github.com>
2024-03-12 10:08:06 +0100

    add title

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: 'Homepage van je Huis',
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { charset: 'utf-8' },
